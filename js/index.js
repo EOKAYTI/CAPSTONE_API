7ad23d3ab96a6271ebec7010d895534eb0b3b8fa
tuan <langkhaituan26@gmail.com>
2024-10-10 11:22:38 +0700

    get all productS from API

---
+++ js/index.js
@@ -0,0 +1,58 @@
+// LẤY DANH SÁCH SẢN PHẨM
+function getProduct() {
+  let promise = axios({
+    // url : Request URLs
+    url: "https://shop.cyberlearn.vn/api/Product",
+    // method : Phương thức (GET - POST - PUT - DELETE)
+    method: "GET",
+  });
+
+  // thành công .then(callback funtion) | thất bại .catch(callback funtion)
+  promise
+    .then((res) => {
+      console.log(res.data.content);
+      renderDataProduct(res.data.content);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+getProduct();
+
+// HIỂN THỊ SẢN PHẨM
+function renderDataProduct(arr) {
+  let content = "";
+  for (let sanPham of arr) {
+    const {
+      id,
+      name,
+      alias,
+      price,
+      description,
+      size,
+      shortDescription,
+      quantity,
+      deleted,
+      categories,
+      relatedProducts,
+      feature,
+      image,
+    } = sanPham;
+    content += `
+    <div class="col-3">
+        <div class="product_item">
+            <div class="product_img">
+                <img src=${image} alt="" />
+            </div>
+            <div class="product_content">
+                <h6>${name}</h6>
+                <p>${size}</p>
+                <p class="product_price">${price} $</p>
+            </div>
+        </div>
+    </div>
+    `;
+  }
+  document.getElementById("add").innerHTML = content;
+}
